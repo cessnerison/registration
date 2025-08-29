69d65096cbb883db230422aa7467e4dd130db930
CL3-PC35 <CL3-PC35@DESKTOP-Q4JU49M>
2025-08-29 15:45:13 +0800

    RPS

---
+++ mavenproject3/average.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/*
+import java.util.Scanner;
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author CL3-PC35
+ */
+import java.util.Scanner;
+public class average {
+   
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+       System.out.print("Enter Your 1st Quarter:");
+        int st = input.nextInt();
+        System.out.print("Enter Your 2nd Quarter:");
+        int nd = input.nextInt();
+    }
+}
