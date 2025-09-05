e5630007e07b17890a785f32d9ead9e4073dba74
CL3-PC35 <CL3-PC35@DESKTOP-Q4JU49M>
2025-09-05 15:09:30 +0800

    RPS

---
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/cafeteria.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.mavenproject3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author CL3-PC35
+ */
+public class cafeteria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+      System.out.print("cafeteria");
+      System.out.print("Snack");
+      System.out.print("[1] - Hotdog 25.00");
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter your choice");
+      int choice = input.nextInt();
+      if(choice ==1)
+      {
+          
+      }
+      
+    
+    
+    
+    }
+}
