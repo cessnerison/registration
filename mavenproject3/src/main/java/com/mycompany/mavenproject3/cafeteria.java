e00363c60121a1c9b14876a146b79125b9c3ed26
CL3-PC35 <CL3-PC35@DESKTOP-Q4JU49M>
2025-09-12 13:51:45 +0800

    RPS

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/cafeteria.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/cafeteria.java
@@ -4,31 +4,56 @@
  */
 package com.mycompany.mavenproject3;
 
-import java.util.Scanner;
+
 
 /**
  *
  * @author CL3-PC35
  */
+import java.util.Scanner;
 public class cafeteria {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-      System.out.print("cafeteria");
-      System.out.print("Snack");
-      System.out.print("[1] - Hotdog 25.00");
+      System.out.println("cafeteria");
+      System.out.println("Snack");
+      System.out.println("[1] - Hotdog 25.00");
       Scanner input = new Scanner(System.in);
+      
       System.out.print("Enter your choice");
       int choice = input.nextInt();
+      int cash = 0;
+      
       if(choice ==1)
       {
+          System.out.println("hotdog");
+          System.out.println("25.00");
+          int hotdog = 25;
+          System.out.println("Enter Quantity");
+          int quantity = input.nextInt();
+          int semtot = quantity * hotdog;
+          System.out.println("Your semi total is:" + semtot );
           
+          while(cash < semtot) {
+           System.out.println("Enter Payment");
+           cash = input.nextInt();   
+          }
+        int change = cash - semtot;
+        if(change > 0){
+           System.out.println("Payment Succesful");
+           System.out.println("Your Change is:" + change);
+        
+        }
+        else{
+        System.out.println("Payment Succesful");
+         System.out.println("No Change");
+        }
       }
-      
-    
-    
-    
     }
 }
+      
+     
+      
+
